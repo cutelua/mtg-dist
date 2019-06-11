c21760fe2487f9c3c96c95d3fbd8ade122d2e2d3
cutelua <33216131+cutelua@users.noreply.github.com>
2019-06-11 14:44:18 +0800

    gitver

--- makebin.sh
+++ makebin.sh
@@ -3,5 +3,7 @@ PKGNAME="mtg-dist"
 PKGDESC="Bullshit-free MTPROTO proxy Bin package [build $(date '+%Y-%m-%d')]"
 START="./setup.sh"
 BASEDIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+
 cd $BASEDIR
-makeself . ../$PKGNAME-$(date '+%Y%m%d-%H%M%S').bin "$PKGDESC" $START
+GITVER=$(git describe --tags || git rev-parse --short HEAD)
+makeself . ../$PKGNAME-$GITVER-$(date '+%Y%m%d-%H%M%S').bin "$PKGDESC" $START
