b4a778927db4b9ea47d1f707d11db8886bc1f5cf
cutelua <33216131+cutelua@users.noreply.github.com>
2019-06-12 10:18:38 +0800

    fix wget args

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Binary dist script for mtg ( https://github.com/9seconds/mtg ).
 
 # Install
 ```
-bash <(wget -O - https://git.io/mtg.sh)
+bash <(wget -qO - https://git.io/mtg.sh)
 ```
 
 # Compile
--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ set -o pipefail
 set -o nounset
 
 TMPDIR=$(mktemp -d)
+trap 'echo Signal caught, cleaning up >&2; cd /; rm -rf "\$TMPDIR"; exit 15' 1 2 3 15
+
 BIN=$TMPDIR/mtg.bin
-wget -q -O - https://api.github.com/repos/cutelua/mtg-dist/releases/latest \
+wget -qO - https://api.github.com/repos/cutelua/mtg-dist/releases/latest \
 | grep browser_download_url | cut -d '"' -f 4 | wget -q -i - -O $BIN
 bash $BIN
-rm -f $TMPDIR
+rm -rf $TMPDIR
