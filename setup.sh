35c7e3de262b00e7b104639d6cdf732f16fe7827
cutelua <33216131+cutelua@users.noreply.github.com>
2019-12-26 14:04:46 +0800

    use mtg log tme_url

--- setup.sh
+++ setup.sh
@@ -96,8 +96,8 @@ sleep 2
 echo "> Generated Secret: ${SECRET}"
 echo "> Mtg listening at port: ${PORT}"
 echo ">  ..."
-SADDR=$(wget -qO- -4 https://www.cloudflare.com/cdn-cgi/trace | grep 'ip=' | cut -d= -f2)
-echo "> Setup mtproxy in telegram with this URL: "
-echo "https://t.me/proxy?server=${SADDR}&port=${PORT}&secret=${SECRET}"
+#SADDR=$(wget -qO- -4 https://www.cloudflare.com/cdn-cgi/trace | grep 'ip=' | cut -d= -f2)
+echo "> Setup mtproxy in telegram with following URL: "
+journalctl -u mtg --since today | grep tme_url
 echo ""
 echo "> Bye."
